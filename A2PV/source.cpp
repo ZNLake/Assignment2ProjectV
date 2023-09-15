17fd11228f3823afb08d11d2e295fce5d331a889
ZNLake <91035765+ZNLake@users.noreply.github.com>
2023-09-15 15:41:44 -0400

    added PRE_RELEASE directive and corresponding runnable code.

--- A2PV/source.cpp
+++ A2PV/source.cpp
@@ -5,12 +5,23 @@
 #include <string>
 #include <vector>
 
+#ifdef _DEBUG
+#define LOG(x) std::cout << "MACRO: " << x << endl;
+#define PRE_RELEASE
+#else
+#define LOG(x)
+#endif // _DEBUG
+
 using namespace std;
 
 struct STUDENT_DATA
 {
 	string firstName;
 	string lastName;
+
+#ifdef PRE_RELEASE
+	string email;
+#endif
 };
 
 int main()
@@ -20,19 +31,37 @@ int main()
 	vector<STUDENT_DATA> v1;
 	string lineText;
 
-	readFile.open("StudentData.txt");
+#ifdef main
+	LOG("running standard code.")
+		readFile.open("StudentData.txt");
+#endif
+
+#ifdef PRE_RELEASE
+	LOG("running PreRelease code.")
+		readFile.open("StudentData_Emails.txt");
+#endif
 
 	while (getline(readFile, lineText))
 	{
 		STUDENT_DATA student;
 
-		int commaLocation = lineText.find(',');
-
+		int commaLocation = lineText.find_first_of(',');
 		student.firstName = lineText.substr(0, commaLocation);
+
+#ifdef main
 		student.lastName = lineText.substr(commaLocation + 1);
+#endif
+
+#ifdef PRE_RELEASE
+		int commaLocation2 = lineText.find_last_of(',');
+		student.lastName = lineText.substr(commaLocation + 1, commaLocation2);
+		student.email = lineText.substr(commaLocation2 + 1);
+#endif
+
 		v1.push_back(student);
 	}
 
 	readFile.close();
+
 	return 1;
 }
