b7a7fceec7ea28c8873b00bc92e7d841389fca48
ZNLake <91035765+ZNLake@users.noreply.github.com>
2023-09-15 14:48:32 -0400

    StudentData.txt successfully loaded.

--- A2PV/source.cpp
+++ A2PV/source.cpp
@@ -1,4 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <thread>
+#include <chrono>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct STUDENT_DATA
+{
+	string firstName;
+	string lastName;
+};
+
 int main()
 {
+	ifstream readFile;
+
+	vector<STUDENT_DATA> v1;
+	string lineText;
+
+	readFile.open("StudentData.txt");
+
+	while (getline(readFile, lineText))
+	{
+		STUDENT_DATA student;
+
+		int commaLocation = lineText.find(',');
+
+		student.firstName = lineText.substr(0, commaLocation);
+		student.lastName = lineText.substr(commaLocation + 1);
+		v1.push_back(student);
+	}
+
+	readFile.close();
 	return 1;
 }
